fdf830f32060d5a1616a489543a6c0fdf3a531bc
jackyliu <18922251299@163.com>
2023-02-17 15:43:35 +0800

    update(JAVA_logger): result manager system

--- JAVA_Logger/Logger_Example.java
+++ JAVA_Logger/Logger_Example.java
@@ -3,15 +3,18 @@ public class Logger_Example {
   public static void main(String[] args) {
     System.out.println("Normal Output");
     System.out.println("===== ===== ===== =====");
-    testLog();
+    test();
     log.setLogLevel(LogLevel.Debug);
     System.out.println("===== ===== ===== =====");
-    testLog();
+    test();
     log.setLogLevel(LogLevel.Error);
     System.out.println("===== ===== ===== =====");
-    testLog();
+    test();
   }
 
+  public static void test() {
+    testLog();
+  }
   public static void testLog() {
     log.error("error");
     log.warn("warning");
